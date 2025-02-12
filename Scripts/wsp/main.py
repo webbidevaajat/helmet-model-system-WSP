e2c23a1b672c1d48808d0fb97d01758d3ff74339
abdulrr <76995664+abdulrr@users.noreply.github.com>
2025-02-12 12:48:19 +0200

    Add Emme paths to main

--- Scripts/wsp/main.py
+++ Scripts/wsp/main.py
@@ -1,6 +1,14 @@
+import sys
+from os import path
+from time import process_time
+sys.path.append("C:/Program Files/Bentley/OpenPaths/EMME 24.01.00/Python311"\
+    "/Lib/site-packages")
+sys.path.append("C:/Program Files/Bentley/OpenPaths/EMME 24.01.00/Python311"\
+    "/Lib/site-packages/win32/lib")
+sys.path.append("C:/Program Files/Bentley/OpenPaths/EMME 24.01.00/Python311"\
+    "/Lib/site-packages/win32/lib")
 import inro.emme.desktop.app as _app
 import inro.modeller as _m
-from time import process_time
 from pathlib import Path
 
 from line_statistics import get_transit_stats
@@ -8,15 +16,16 @@ from line_statistics import get_transit_stats
 
 def main():
     time_start = process_time()
+    
 
-    emme_proj_path = Path("C:/emmeproj/HELMET41/emme_testnetwork")
-    emp_name = "Linjasto_2040.emp"
+    emme_proj_path = Path("C:/EMME projects/Helmet testaus/Helmet_testaus/")
+    emp_name = "Helmet_testaus.emp"
     emmepath = emme_proj_path / emp_name
     desktop = _app.start_dedicated(
         project=emmepath, visible=False, user_initials="WSP")
     modeller = _m.Modeller(desktop)
-    results_path = Path(__file__).parent / "results"
-    run_scens = ["VE0 2024"]
+    results_path = emme_proj_path / "Output"
+    run_scens = ["Helmet 4", "Helmet 5"]
     get_transit_stats(run_scens, modeller, results_path)
 
     time_stop = process_time()
