7ddadfbfb6c5931c0451bf6842572bf5c2225c14
abdulrr <76995664+abdulrr@users.noreply.github.com>
2025-02-12 19:35:30 +0200

    Export comparison tables
    Export comparisons for boardings by mode and number of trips by area

--- Scripts/wsp/compare.py
+++ Scripts/wsp/compare.py
@@ -1,127 +1,100 @@
 import pandas as pd
-import bokeh as bk
-from bokeh.plotting import figure, output_file, save
-from bokeh.io import show
-from bokeh.transform import dodge
-from bokeh.models import ColumnDataSource
+import config_scens
+from pathlib import Path
 
-def get_helmet_boarding_results(scenarios: list, results_path: Path)
+conf_scens = config_scens.scenarios
+
+def get_helmet_boarding_by_mode(scenarios: list, results_path: Path):
     """This function reads the csv files for boardings and compares results.
     """
     helmet_results = pd.DataFrame()
     
     for scenario in scenarios:
-        scenario_results_path = results_path / f"transit_stats_{scenario['year']}_{scenario['name']}.csv"
-        results = pd.read_csv(scenario_results_path)
-        #results = results[['', '']]
+        scenario = conf_scens[scenario]
+        scenario_results_path = results_path / f"transit_stats_{scenario['year']}_{scenario['name']}_volumes.csv"
+        results = pd.read_csv(scenario_results_path, sep=';')
         results['scenario'] = scenario['name']
-        helmet_results = pd.concat([helmet_results, data], ignore_index=True)
+        helmet_results = pd.concat([helmet_results, results], ignore_index=True)
+    return helmet_results
+
+def get_helmet_boarding_by_area(scenarios: list, results_path: Path):
+    """This function reads the csv files for boardings and compares results.
+    """
+    helmet_results = pd.DataFrame()
+    
+    for scen in scenarios:
+        scenario = conf_scens[scen]
+        scenario_results_path = results_path / f"transit_stats_{scenario['year']}_{scenario['name']}_area_boardings.csv"
+        results = pd.read_csv(scenario_results_path, sep=';')
+        results['scenario'] = scenario['name']
+        helmet_results = pd.concat([helmet_results, results], ignore_index=True)
 
     return helmet_results
 
 def compare_by_mode(scenarios: list, results_path: Path, compare_data: Path):
-    """This function compares the boarding totals of Helmet with 
-    real boarding statistics by mode.
+    """This function compares Helmet boarding totals by mode with  
+    HSL statistics.
     """
+    helmet_boardings = pd.DataFrame()
 
-    helmet_boardings = get_helmet_boarding_results(scenarios, results_path)
+    helmet_boardings = get_helmet_boarding_by_mode(scenarios, results_path)
+    helmet_boardings['transit_boa'] = helmet_boardings['transit_boa'] / 1000000
+
     hsl_boardings = pd.read_csv(compare_data)
 
     # join dataframes
-    helmet_boardings[['mode_name', 'mode', 'nousijat_2023']]
-    helmet_boardings = helmet_boardings.rename(columns={'nousijat_2023': 'boardings'})
-    hsl_boardings['scenario'] = 'HSL'
+    hsl_boardings = hsl_boardings[['mode', 'nousijaa_2023']]
+    hsl_boardings = hsl_boardings.rename(columns={'nousijaa_2023': 'transit_boa'})
+    hsl_boardings['scenario'] = 'HSL_2023'
 
-    # Perform inner join on different column names
-    merged_data = pd.merge(helmet_boardings, hsl_boardings, left_on='mode', right_on='mode', how='inner')
-    
-    # Group by mode and source, then sum the boardings
-    grouped_data = merged_data.groupby(['mode_name', 'mode', 'scenario']).sum().reset_index()
+    # join dataframes
+    joined_data = pd.concat([hsl_boardings, helmet_boardings], ignore_index=True)
 
     # Pivot the data to have sources as columns
-    pivot_data = grouped_data.pivot(index='mode_name', columns='scenario', values='boardings')
+    pivot_data = joined_data.pivot(index='mode', columns='scenario', values='transit_boa')
 
-    # Prepare data for Bokeh
-    pivot_data = pivot_data.reset_index()
-    source = ColumnDataSource(pivot_data)
+    # Add column name 
+    mode_names = pd.read_csv(compare_data)
+    mode_names = mode_names[['mode', 'mode_name']].drop_duplicates()
+    
+    pivot_data = pivot_data.reset_index().merge(mode_names, on='mode', how='left').set_index('mode')
+    cols = ['mode_name'] + [col for col in pivot_data if col != 'mode_name']
+    pivot_data = pivot_data[cols]
 
-    # Define output file
-    output_file("boarding_totals_by_mode.html")
+    # Export results comparison by mode
+    filename = f"transit_boardings_comparison.csv"
+    savefile = results_path / filename
+    
+    # Export results comparison by mode
+    pivot_data.to_csv(savefile, sep=";", index=True)
+    
+    print(f"Successfully exported {filename}")
 
-    # Create a figure
-    p = figure(x_range=pivot_data['mode_name'], plot_height=400, plot_width=800, title="Miljoonaa nousijaa",
-               toolbar_location=None, tools="")
-
-    # Add bars for each scenario
-    scenarios = pivot_data.columns[1:]
-    colors = ["#c9d9d3", "#718dbf", "#e84d60"]
-    for i, scenario in enumerate(scenarios):
-        p.vbar(x=dodge('mode_name', -0.25 + i*0.25, range=p.x_range), top=scenario, width=0.2, source=source,
-               color=colors[i], legend_label=scenario)
-
-    # Customize plot
-    p.xgrid.grid_line_color = None
-    p.y_range.start = 0
-    p.xaxis.axis_label = "Kulkumuoto"
-    p.yaxis.axis_label = "Miljoonaa nousijaa"
-    p.xaxis.major_label_orientation = 1.2
-    p.legend.title = "Data"
-    p.legend.location = "top_left"
-
-    # Save and show the plot
-    save(p)
-    show(p)
    
-def compare_by_area(scenarios: list, results_path: Path, compare_data: Path):
+def compare_by_area(scenarios: list, results_path: Path):
     """This function compares the boarding totals of Helmet with 
     real boarding statistics by area.
     """
 
-    helmet_boardings = get_helmet_boarding_results(scenarios, results_path)
-    hsl_boardings = pd.read_csv(compare_data)
+    helmet_boardings = pd.DataFrame()
+
+    helmet_boardings = get_helmet_boarding_by_area(scenarios, results_path)
 
     # join dataframes
-    helmet_boardings[['area_name', 'area', 'nousijat_2023']]
-    helmet_boardings = helmet_boardings.rename(columns={'nousijat_2023': 'boardings'})
-    hsl_boardings['scenario'] = 'HSL'
-
-    # Perform inner join on different column names
-    merged_data = pd.merge(helmet_boardings, hsl_boardings, left_on='area', right_on='area', how='inner')
+    helmet_boardings[['scenario', 'area', 'first_boa']]
     
-    # Group by area and source, then sum the boardings
-    grouped_data = merged_data.groupby(['area_name', 'area', 'scenario']).sum().reset_index()
+    # Group by mode and source, then sum the boardings
+    grouped_data = helmet_boardings.groupby(['scenario', 'area']).sum().reset_index()
+    
+    # Convert boardings to millions
+    grouped_data['miljoonaa_matkat'] = grouped_data['first_boa'] / 1000000
 
     # Pivot the data to have sources as columns
-    pivot_data = grouped_data.pivot(index='area_name', columns='scenario', values='boardings')
+    pivot_data = grouped_data.pivot(index='area', columns='scenario', values='miljoonaa_matkat')
 
-    # Prepare data for Bokeh
-    pivot_data = pivot_data.reset_index()
-    source = ColumnDataSource(pivot_data)
-
-    # Define output file
-    output_file("boarding_totals_by_area.html")
-
-    # Create a figure
-    p = figure(x_range=pivot_data['area_name'], plot_height=400, plot_width=800, title="Miljoonaa nousijaa",
-               toolbar_location=None, tools="")
-
-    # Add bars for each scenario
-    scenarios = pivot_data.columns[1:]
-    colors = ["#c9d9d3", "#718dbf", "#e84d60"]
-    for i, scenario in enumerate(scenarios):
-        p.vbar(x=dodge('area_name', -0.25 + i*0.25, range=p.x_range), top=scenario, width=0.2, source=source,
-               color=colors[i], legend_label=scenario)
-
-    # Customize plot
-    p.xgrid.grid_line_color = None
-    p.y_range.start = 0
-    p.xaxis.axis_label = "Alue"
-    p.yaxis.axis_label = "Miljoonaa nousijaa"
-    p.xaxis.major_label_orientation = 1.2
-    p.legend.title = "Data"
-    p.legend.location = "top_left"
-
-    # Save and show the plot
-    save(p)
-    show(p)
+    # Export results comparison by mode
+    filename = f"transit_trips_comparison.csv"
+    savefile = results_path / filename
+    pivot_data.to_csv(savefile, sep=";", index=True)
+    print(f"Successfully exported {filename}")
    
--- Scripts/wsp/main.py
+++ Scripts/wsp/main.py
@@ -6,6 +6,7 @@ from time import process_time
 import config_scens
 from line_statistics import get_transit_stats
 from calc_dist_distribution import get_dd_data
+from compare import compare_by_mode, compare_by_area
 
 
 def main():
@@ -18,12 +19,15 @@ def main():
         project=emmepath, visible=False, user_initials="WSP")
     modeller = _m.Modeller(desktop)
     results_path = emme_proj_path / "Output"
+    hsl_statistics = Path("//corp.pbwan.net/fi/FIHEL300/1000/321609_Helmet_5_testaus/Emme/Lahtodata/")
 
     run_scens = [scen_name for scen_name in config_scens.scenarios]
     dist_bins = [0, 1, 3, 5, 7, 10, 15, 25, 50]
     
     get_transit_stats(run_scens, modeller, results_path)
     get_dd_data(run_scens, modeller, dist_bins, results_path)
+    compare_by_mode(run_scens, results_path, hsl_statistics / "HSL_stats_mode.csv")
+    compare_by_area(run_scens, results_path)
 
     time_stop = process_time()
     print(f"----- Program execution time {time_stop - time_start} seconds")
